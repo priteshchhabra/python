400611e63fe6b9c457aef0300c22e5030872deb4
priteshchhabra <36761974+priteshchhabra@users.noreply.github.com>
2018-02-23 16:53:14 +0530

    Add files via upload

---
+++ max.py
@@ -0,0 +1,13 @@
+a=input("enter a : ")
+b=input("enter b : ")
+c=input("enter c : ")
+def max():
+    if a>b and b>c :
+        print(a,"is the largest")
+    if b>a and b>c :
+        print(b ,"is the largest")
+    if c>a and c>b :
+        print(c,"is the largest")
+
+
+max()
